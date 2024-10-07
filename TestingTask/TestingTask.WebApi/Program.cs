284c05348a42d7fd257eba9b911e39af08f4c36f
Alexandr Chumakov <Alex1234014@gmail.com>
2024-10-07 14:54:17 +0500

    #1 -create solution and add WebApi project

---
+++ TestingTask/TestingTask.WebApi/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
+
