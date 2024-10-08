026165ee2afcf7694efd3686e704be31f8d87286
Alexandr Chumakov <Alex1234014@gmail.com>
2024-10-08 15:38:41 +0500

    #2 -add user models and update db User model

--- TestingTask/Domain/Authentication/Models/User.cs
+++ TestingTask/Domain/Authentication/Models/User.cs
@@ -2,7 +2,7 @@ namespace Domain.Authentication.Models;
 
 public sealed class User
 {
-    public Guid? Id { get; set; }
-    public string Phone { get; set; } 
+    public int Id { get; set; }
+    public string Phone { get; set; }
     public string Password { get; set; }
 }
---
+++ TestingTask/WebApi/Authentication/DTOs/UserLoginDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using TestingTask.WebApi.Shared.Resources;
+
+namespace TestingTask.WebApi.Authentication.DTOs;
+
+public class UserLoginDto
+{
+    [RegularExpression(@"^\d{10}$", ErrorMessageResourceType = typeof(Responses),
+        ErrorMessageResourceName = "Format_phone")]
+    [Required(ErrorMessageResourceType = typeof(Responses), ErrorMessageResourceName = "Required_phone")]
+    public required string Phone { get; set; }
+
+    [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]?).{9,}$",
+        ErrorMessageResourceType = typeof(Responses),
+        ErrorMessageResourceName = "Format_pass")]
+    [Required(ErrorMessageResourceType = typeof(Responses), ErrorMessageResourceName = "Required_pass")]
+    public required string Password { get; set; }
+}
