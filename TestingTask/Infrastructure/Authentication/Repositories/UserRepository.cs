2b7bbef563e1d27b80c0beba6b0cb9315534e9de
Alexandr Chumakov <Alex1234014@gmail.com>
2024-10-08 15:39:17 +0500

    #2 -fix bugs into UserRepository.cs

--- TestingTask/Infrastructure/Authentication/Repositories/UserRepository.cs
+++ TestingTask/Infrastructure/Authentication/Repositories/UserRepository.cs
@@ -8,28 +8,28 @@ namespace Infrastructure.Authentication.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    private const string ConnString = "Host=localhost;Port=5431;Database=testedProj;Username=postgres;Password=password";
+    private const string ConnString =
+        "Host=localhost;Port=5431;Database=testedProj;Username=postgres;Password=password";
 
-    private IDbConnection Connection => new NpgsqlConnection(ConnString);
+    private static IDbConnection Connection => new NpgsqlConnection(ConnString);
 
     public async Task<User?> GetByPhoneAsync(string phone)
     {
         using var dbConnection = Connection;
-        var user = await Connection.QueryFirstOrDefaultAsync<User?>($"SELECT * FROM users WHERE phone = {phone}");
+        var user = await Connection.QueryFirstOrDefaultAsync<User?>(
+            "SELECT * FROM users WHERE phone = @Phone::varchar", new { Phone = phone });
         return user;
     }
 
     public async Task CreateAsync(User user)
     {
-        const string sql = "INSERT INTO users (phone, password) VALUES {@Phone, @Password}";
-        object[] param = [new { Phone = user.Phone, Password = user.Password }];
-        await ExecuteAsync(sql, param);
+        const string sql = "INSERT INTO users (phone, password) VALUES (@Phone, @Password)";
+        await ExecuteAsync(sql, new { user.Phone, user.Password });
     }
 
-    private async Task ExecuteAsync(string sql, object param)
+    private static async Task ExecuteAsync(string sql, object param)
     {
         using var dbConnection = Connection;
         await Connection.ExecuteAsync(sql, param);
     }
 }
-
