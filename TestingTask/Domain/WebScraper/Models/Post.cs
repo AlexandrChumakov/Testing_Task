b7f0a56ffc5fc1636c0e2863915f4ff72d4ae8bf
Alexandr Chumakov <Alex1234014@gmail.com>
2024-11-14 17:43:38 +0500

    -update Post model and dockers files

--- TestingTask/Domain/WebScraper/Models/Post.cs
+++ TestingTask/Domain/WebScraper/Models/Post.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace Domain.WebScraper.Models;
 
 public struct Post
@@ -7,4 +9,11 @@ public struct Post
     public string Description { get; set; }
 
     public DateTime Date { get; set; }
+
+
+    public override string ToString()
+    {
+        return
+            $"<b> {Title} </b> \n \n  <b> Описание: </b> <i> {Description} </i>   \n \n <b> Дата публикации: </b> {Date.Date}";
+    }
 }
